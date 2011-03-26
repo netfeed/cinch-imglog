e59a4ff6013781c0b8264142d18290e641d8295f
Victor Bergöö <victor.bergoo@gmail.com>
2011-03-26 16:49:15 +0100

    copyright notice

--- lib/cinch/plugins/imglog.rb
+++ lib/cinch/plugins/imglog.rb
@@ -1,3 +1,8 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011 Victor Bergöö
+# This program is made available under the terms of the MIT License.
+
 require 'cinch'
 require 'curl'
 require 'uri'
